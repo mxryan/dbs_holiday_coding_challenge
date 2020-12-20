1cf7e38c208be80bf59c4125543738e84e1f537b
michaelryan <mxryan@live.com>
2020-12-20 09:21:50 -0600

    reworked to immediately ask for country codes

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ const API_ROOT: &str = "https://holidayapi.com/v1/holidays";
 const YEAR: &str = "2019";
 const API_KEY: &str = "4375217a-5fd6-4b2a-8258-9b5bf543b7cc";
 const DEFAULT_COUNTRY_CODE: &str = "US";
+const ASK_FOR_COUNTRY_CODES_MSG: &str = "Enter country codes separated by spaces. A maximum of 3 country codes are supported at this time";
 
 #[derive(Serialize, Deserialize, Debug)]
 struct HolidayApiResponseBody {
@@ -107,23 +108,28 @@ fn pretty_print_json(api_data: &HolidayApiResponseBody) {
 }
 
 fn main() -> Result<()> {
-    let selected_option = get_starting_input();
-    match selected_option {
-        Ok(1) => {
-            match get_input("Enter a country code: ") {
-                Ok(country_code) => {
-                    match fetch_data(&country_code) {
-                        Ok(api_data) => println!("Data received"),
-                        Err(e) => println!("Error: {}", e),
+    match get_country_inputs() {
+        Ok(country_codes) => {
+            println!("You gave me these country codes: {:?}", country_codes);
+            for country_code in country_codes.iter() {
+                match fetch_data(country_code) {
+                    Ok(data) => {
+                        // process data
+                        let stats = HolidayStats::from(&data.holidays);
+                        println!("Found data for {}", country_code);
+                    }
+                    Err(e) => {
+                        println!(
+                            "Failed to fetch data for {}, error: {}",
+                            country_code, e
+                        );
                     }
                 }
-                Err(e) => println!("Error: {}", e),
             }
         }
-        Ok(2) => println!("TODO: ENTER TWO COUNTRY CODES"),
-        Ok(3) => println!("See you later."),
-        Ok(_) => println!("Not a valid selection. Exiting."),
-        Err(e) => println!("Error: {}", e),
+        Err(e) => {
+            println!("Error: {}", e);
+        }
     }
     Ok(())
 }
@@ -142,18 +148,19 @@ fn fetch_data(country_code: &str)
     Ok(body)
 }
 
-fn get_starting_input() -> std::result::Result<i8, Box<dyn Error>> {
-    println!("Select one of the following: ");
-    println!("1) I want to know how many holidays there were in 2019 in a country");
-    println!("2) I want to compare two different countries' holidays from 2019");
-    println!("3) Exit.");
-    println!("Enter selection: ");
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    let result: i8 = input.trim().parse()?;
-
-    Ok(result)
+fn get_country_inputs()
+    -> std::result::Result<Vec<String>, Box<dyn Error>>
+{
+    let user_input = get_input(ASK_FOR_COUNTRY_CODES_MSG)?;
+    let xyz: Vec<String> = user_input
+        .trim()
+        .split_whitespace()
+        .map(|x| { String::from(x.trim()) })
+        .collect();
+    if xyz.len() > 3 {
+        panic!("Only three countries are supported at this time");
+    }
+    Ok(xyz)
 }
 
 fn get_input(msg: &str) -> std::result::Result<String, Box<dyn Error>> {
